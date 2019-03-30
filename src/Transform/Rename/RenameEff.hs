a4f402db4250206c76f24393cec89862be1a144b
Albie Baker-Smith <bakersmitha@gmail.com>
2019-03-30 13:59:40 +0000

    separate names for variables and procdures

--- src/Transform/Rename/RenameEff.hs
+++ src/Transform/Rename/RenameEff.hs
@@ -17,28 +17,48 @@ import Helper.Prog
 import Helper.Co
 import Helper.Eff
 
+--------------------------------------------------------------------------------
+-- Syntax
+--------------------------------------------------------------------------------
+
 type Fresh = Word
 
+type VarNames  = [Ident]
+type ProcNames = [Ident]
+
 data Rename k
-    -- Takes original name and returns fresh name.
-    = Name' Ident (Fresh -> k)
+    -- Returns fresh name corresponding to renamed identifier. If the variable
+    -- is unseen before (i.e. global variable), then a fresh mapping is created.
+    = VarName' Ident (Fresh -> k)
+    -- Returns fresh name corresponding to renamed procedure.
+    | ProcName' Ident (Fresh -> k)
     deriving Functor
 
 data Local k
     -- Assigns fresh names to all supplied names, and uses these new names
     -- inside continuation. After local, original names are restored.
-    = Local' [Ident] k
+    = Local' VarNames ProcNames k
     deriving Functor
 
 -- Smart constructors
 
-pattern Name v fk <- (prj -> Just (Name' v fk))
-name :: (Functor f, Functor g) => Rename :<: f => Ident -> Prog f g Fresh
-name v = inject (Name' v Var)
+-- Use pattern synonyms and view patterns suggested in Effect Handlers in Scope.
+-- These help make pattern matching in effect handler more readable.
+pattern VarName v fk <- (prj -> Just (VarName' v fk))
+varName :: (Functor f, Functor g) => Rename :<: f => Ident -> Prog f g Fresh
+varName v = inject (VarName' v Var)
 
-pattern Local vs k <- (prj -> Just (Local' vs k))
-local :: (Functor f, Functor g) => Local :<: g => [Ident] -> Prog f g a -> Prog f g a
-local vs inner = injectS (fmap (fmap return) (Local' vs inner))
+pattern ProcName v fk <- (prj -> Just (ProcName' v fk))
+procName :: (Functor f, Functor g) => Rename :<: f => Ident -> Prog f g Fresh
+procName v = inject (ProcName' v Var)
+
+pattern Local vs ps k <- (prj -> Just (Local' vs ps k))
+local :: (Functor f, Functor g) => Local :<: g => VarNames -> ProcNames -> Prog f g a -> Prog f g a
+local vs ps inner = injectS (fmap (fmap return) (Local' vs ps inner))
+
+--------------------------------------------------------------------------------
+-- Semantics Auxillary
+--------------------------------------------------------------------------------
 
 -- Keeps track of mappings from original to fresh names, as well as the next
 -- avaiable fresh name.
@@ -78,42 +98,78 @@ restoreNames old new = Names seen' next' where
     seen' = seen old `Map.union` seen new
     next' = next new -- Don't want to restore next fresh name
 
--- Same as State from EffectHandler.hs
--- I.e. compositional effect handler, allowing renaming syntax to be mixed
--- with AST syntax.
+-- Simple wrapper around Names to store both variable and procedure names.
+-- Must be kept separate to avoid overwriting.
+data Env = Env {
+    varNames  :: Names
+  , procNames :: Names
+}
+
+emptyEnv :: Env
+emptyEnv = Env emptyNames emptyNames
+
+getFreshVar :: Ident -> Env -> (Fresh, Env)
+getFreshVar v env = (fresh, env { varNames=varNames' }) where
+    (fresh, varNames') = getFresh v (varNames env)
+
+getFreshProc :: Ident -> Env -> (Fresh, Env)
+getFreshProc f env = (fresh, env { procNames=procNames' } ) where
+    (fresh, procNames') = getFresh f (procNames env)
+
+addManyFreshVsPs :: VarNames -> ProcNames -> Env -> Env
+addManyFreshVsPs vs ps env = env { varNames=varNames', procNames=procNames' } where
+    varNames'  = addManyFresh vs (varNames env)
+    procNames' = addManyFresh ps (procNames env)
+
+restoreEnv :: Env -> Env -> Env
+restoreEnv old new = Env vs ps where
+    vs = restoreNames (varNames old) (varNames new)
+    ps = restoreNames (procNames old) (procNames new)
+
+--------------------------------------------------------------------------------
+-- Effect Handler
+--------------------------------------------------------------------------------
+
+-- Same idea State from Syntax and Semantics for Operations with Scope.
 data CarrierRN f g a n
-    = RN { runRN :: Names -> (Prog f g (CarrierRN' f g a n, Names)) }
+    = RN { runRN :: Env -> (Prog f g (CarrierRN' f g a n, Env)) }
 
 data CarrierRN' f g a :: Nat -> * where
-    CZRN :: a -> CarrierRN' f g a 'Z
-    CSRN :: (Names -> (Prog f g (CarrierRN' f g a n, Names))) -> CarrierRN' f g a ('S n)
+    CZ :: a -> CarrierRN' f g a 'Z
+    CS :: (Env -> (Prog f g (CarrierRN' f g a n, Env))) -> CarrierRN' f g a ('S n)
 
 genRN :: (Functor f, Functor g) => a -> CarrierRN f g a 'Z
-genRN x = RN $ \ns -> (return (CZRN x, ns))
+genRN x = RN $ \env -> (return (CZ x, env))
+
+-- Convenience method for getting the fresh name of a variable or procedure
+-- and 'running' the continuation supplying it the fresh name.
+algGet :: (Env -> (Fresh, Env)) -> (Fresh -> CarrierRN f g a n) -> CarrierRN f g a n
+algGet getFreshId fk = RN $ \env ->
+    let (fresh, env') = getFreshId env
+        carrier'      = fk fresh
+    in runRN carrier' env'
 
 algRN :: (Functor f, Functor g) => Alg (Rename :+: f) (Local :+: g) (CarrierRN f g a)
 algRN = A a d p where
     a :: (Functor f, Functor g) => (Rename :+: f) (CarrierRN f g a n) -> CarrierRN f g a n
-    a (Name v fk) = RN $ \ns ->
-        let (fresh, ns') = getFresh v ns
-        in runRN (fk fresh) ns'
-    a (Other op) = RN $ \ns -> Op (fmap (\(RN runRN) -> runRN ns) op) where
+    a (VarName  ident fk) = algGet (getFreshVar  ident) fk
+    a (ProcName ident fk) = algGet (getFreshProc ident) fk
+    a (Other op)          = RN $ \env -> Op (fmap (\(RN runRN) -> runRN env) op) where
 
     d :: (Functor f, Functor g) => (Local :+: g) (CarrierRN f g a ('S n)) -> CarrierRN f g a n
-    d (Local vs k) = RN $ \ns -> do
-        (CSRN runRN', ns') <- runRN k (addManyFresh vs ns)
-        runRN' (restoreNames ns ns')
-
-    d (Other op) = RN $ \ns -> Scope (fmap (f ns) op) where
-        f :: (Functor f, Functor g) => Names -> CarrierRN f g a ('S n) -> Prog f g (Prog f g (CarrierRN' f g a n, Names))
-        f ns l = fmap g (runRN l ns) where
-            g :: (CarrierRN' f g a ('S n), Names) -> Prog f g (CarrierRN' f g a n, Names)
-            g (CSRN runRN', ns') = runRN' ns'
+    d (Local vs ps k) = RN $ \env -> do
+        (CS runRN', env') <- runRN k (addManyFreshVsPs vs ps env)
+        runRN' (restoreEnv env env')
+    d (Other op) = RN $ \env -> Scope (fmap (f env) op) where
+        f :: (Functor f, Functor g) => Env -> CarrierRN f g a ('S n) -> Prog f g (Prog f g (CarrierRN' f g a n, Env))
+        f env l = fmap g (runRN l env) where
+            g :: (CarrierRN' f g a ('S n), Env) -> Prog f g (CarrierRN' f g a n, Env)
+            g (CS runRN', env') = runRN' env'
 
     p :: (Functor f, Functor g) => CarrierRN f g a n -> CarrierRN f g a ('S n)
-    p (RN runRN) = RN $ \ns -> return (CSRN runRN, ns)
+    p (RN runRN) = RN $ \env -> return (CS runRN, env)
 
 handleRename :: (Functor f, Functor g) => Prog (Rename :+: f) (Local :+: g) a -> Prog f g a
 handleRename prog = do
-    (CZRN prog', _) <- runRN (run genRN algRN prog) emptyNames
+    (CZ prog', _) <- runRN (run genRN algRN prog) emptyEnv
     return prog'
