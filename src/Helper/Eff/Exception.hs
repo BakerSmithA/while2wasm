41eedae5cc882731c4baa93c1e32c6691c2d6a57
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-01 17:15:22 +0100

    not compiling

--- src/Helper/Eff/Exception.hs
+++ src/Helper/Eff/Exception.hs
@@ -47,18 +47,20 @@ catch hdl = injectS (fmap (fmap return) (Catch' hdl (Var ())))
 
 type ErrHdl a = Maybe (String -> a)
 data CarrierExc f g a (n :: Nat)
-    = Exc { runExc :: ErrHdl a -> Prog f g (Either String a) }
+    = Exc { runExc :: ErrHdl (CarrierExc' f g a n) -> Prog f g (Either String (CarrierExc' f g a n)) }
 
 data CarrierExc' f g a :: Nat -> * where
     CZ :: a -> CarrierExc' f g a 'Z
-    CS :: (ErrHdl a -> Prog f g (Either String a)) -> CarrierExc' f g a ('S n)
+    CS :: (ErrHdl (CarrierExc' f g a n) -> Prog f g (Either String a)) -> CarrierExc' f g a ('S n)
 
 genExc :: (Functor f, Functor g) => a -> CarrierExc f g a 'Z
-genExc x = Exc $ \_ -> (return (Right x))
+genExc x = Exc $ \_ -> (return (Right (CZ x)))
 
 algExc :: (Functor f, Functor g) => Alg (Throw :+: f) (Catch :+: g) (CarrierExc f g a)
 algExc = A a d p where
     a :: (Functor f, Functor g) => (Throw :+: f) (CarrierExc f g a n) -> CarrierExc f g a n
+    -- If an error handler exists, use that to handle the error. Otherwise
+    -- simply return the error.
     a (Throw err) = Exc $ \hdl ->
         case hdl of
             Nothing -> return (Left err)
@@ -66,17 +68,37 @@ algExc = A a d p where
     a (Other op) = Exc $ \hdl -> (Op (fmap (\(Exc run) -> run hdl) op))
 
     d :: (Functor f, Functor g) => (Catch :+: g) (CarrierExc f g a ('S n)) -> CarrierExc f g a n
-    d (Catch hdl k) = undefined
+    d (Catch newHdl k) = Exc $ \hdl -> do
+        -- Run continuation with new handler supplied by catch block.
+        r <- runExc k (h newHdl)
+        -- Continue after catch using old handler.
+        either undefined (go hdl) r where
+            go  :: (Functor f, Functor g) => ErrHdl (CarrierExc' f g a n) -> CarrierExc' f g a ('S n) -> Prog f g (Either String (CarrierExc' f g a n))
+            go hdl (CS runExc') = do
+                x <- runExc' hdl
+                case x of
+                    Left err -> undefined
+                    Right y  -> return (Right _)
+
+            --  _ :: Prog f2 g2 (Either String (CarrierExc' f2 g2 a2 n1))
+            --  runExc' :: ErrHdl (CarrierExc' f2 g2 a2 n2) -> Prog f2 g2 (Either String a2)
 
     d (Other op)  = undefined
 
     p :: CarrierExc f g a n -> CarrierExc f g a ('S n)
     p = undefined
 
+    h :: (String -> CarrierExc f g a ('S n)) -> ErrHdl (CarrierExc' f g a ('S n))
+    h = undefined
+
 -- Converts any program which might thrown an exception into a program that
 -- returns either its result, or an error.
 handleExc :: (Functor f, Functor g) => Prog (Throw :+: f) (Catch :+: g) a -> Prog f g (Either String a)
-handleExc prog = runExc (run genExc algExc prog) Nothing
+handleExc prog = do
+    r <- runExc (run genExc algExc prog) Nothing
+    case r of
+        Left err     -> return (Left err)
+        Right (CZ x) -> return (Right x)
 
 test :: Prog (Throw :+: Void) (Catch :+: Void) Int
 test = do
