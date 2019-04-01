5d9fd0c2c8a430c32e8e4dd74d18102f6b4c15a2
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-01 14:17:04 +0100

    started AST_02 but not working

--- src/Transform/Rename/AST_02.hs
+++ src/Transform/Rename/AST_02.hs
@@ -2,13 +2,15 @@
 -- Investigation of methods used to map between representations of the AST.
 -- This method uses extensible effects methods to
 
-{-# LANGUAGE TypeOperators, DataKinds #-}
+{-# LANGUAGE TypeOperators, DataKinds, FlexibleContexts #-}
+{-# LANGUAGE ViewPatterns, PatternSynonyms #-}
 
 module Trans.Rename.AST_02 where
 
 import Front.AST
 import Transform.Rename.RenameEff
 import Helper.Prog
+import Helper.Eff
 import Helper.Eff.Void
 import Helper.Co
 
@@ -20,12 +22,34 @@ type Carrier       f g = CarrierId (RenameHandler f g)
 type RN  f = VarExp Ident :+: VarStm Ident :+: ProcStm Ident :+: f
 type SRN g = BlockStm Ident Ident :+: g
 
-genMk :: () -> Carrier h i 'Z
-genMk = undefined
+-- Define patterns to make alg more readable.
+pattern GetVar' v       <- (prj -> Just (GetVar v))
+pattern SetVar' v  x  k <- (prj -> Just (SetVar v x k))
+pattern Call'   p  k    <- (prj -> Just (Call p k))
+pattern Block'  vs ps b <- (prj -> Just (Block vs ps b))
 
-algMk :: Alg (RN f) (SRN g) (Carrier h i)
-algMk = undefined
+genMk :: (Functor h, Functor i) => () -> Carrier h i 'Z
+genMk x = Id (return (return x))
 
-handleMake :: (Functor f, Functor g) => Prog (RN f) (SRN g) () -> RenameHandler h i
+algMk :: (Functor f, Functor g)
+      => (VarExp Fresh :<: h, VarStm Fresh :<: h, ProcStm Fresh :<: h)
+      => (BlockStm Ident Ident :<: i)
+      => Alg (RN f) (SRN g) (Carrier h i)
+algMk = A a d p where
+    a (GetVar' v) = Id $ do
+        v' <- varName v
+        return (getVar v')
+    a (Other op) = Id (Op (fmap _ op))
+
+    d :: SRN g (Carrier h i ('S n)) -> Carrier h i n
+    d = undefined
+
+    p :: Carrier h i n -> Carrier h i ('S n)
+    p = undefined
+
+handleMake :: (Functor f, Functor g, Functor h, Functor i)
+           => (VarExp Fresh :<: h, VarStm Fresh :<: h, ProcStm Fresh :<: h)
+           => (BlockStm Ident Ident :<: i)
+           => Prog (RN f) (SRN g) () -> RenameHandler h i
 handleMake prog = case run genMk algMk prog of
     (Id hdl) -> hdl
