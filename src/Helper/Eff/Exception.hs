0f388c89c2578d837ddaecf906d1cb218395f72d
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-01 16:24:47 +0100

    working out exec handler

--- src/Helper/Eff/Exception.hs
+++ src/Helper/Eff/Exception.hs
@@ -26,16 +26,16 @@ data Throw k
 
 -- Catch a thrown error of type e.
 data Catch k
-    = Catch' (String -> k)
+    = Catch' (String -> k) k
     deriving Functor
 
 pattern Throw err <- (prj -> Just (Throw' err))
 throw :: Throw :<: f => String -> Prog f g a
 throw err = inject (Throw' err)
 
-pattern Catch hdl <- (prj -> Just (Catch' hdl))
+pattern Catch hdl k <- (prj -> Just (Catch' hdl k))
 catch :: (Functor f, Catch :<: g) => (String -> Prog f g ()) -> Prog f g ()
-catch hdl = injectS (fmap (fmap return) (Catch' hdl))
+catch hdl = injectS (fmap (fmap return) (Catch' hdl (Var ())))
 
 --------------------------------------------------------------------------------
 -- Semantics
@@ -44,31 +44,30 @@ catch hdl = injectS (fmap (fmap return) (Catch' hdl))
 -- Each time a catch is encountered, a new level of scope is entered and
 -- a new handler is added to the top of 'stack'. This is the handler that will
 -- be used if an exception is encountered inside the catch.
---
--- TODO
--- Therefore, the carrier we use is a stack of n error handlers, where Ntohing
--- is used to tie the recursive knot:
---  m (Either a e)
 
+type ErrHdl a = Maybe (String -> a)
 data CarrierExc f g a (n :: Nat)
-    = Exc (Prog f g (Either String a))
+    = Exc { runExc :: ErrHdl a -> Prog f g (Either String a) }
 
 data CarrierExc' f g a :: Nat -> * where
     CZ :: a -> CarrierExc' f g a 'Z
-    CS :: (Prog f g (Either String a)) -> CarrierExc' f g a ('S n)
+    CS :: (ErrHdl a -> Prog f g (Either String a)) -> CarrierExc' f g a ('S n)
 
 genExc :: (Functor f, Functor g) => a -> CarrierExc f g a 'Z
-genExc x = Exc (return (Right x))
+genExc x = Exc $ \_ -> (return (Right x))
 
 algExc :: (Functor f, Functor g) => Alg (Throw :+: f) (Catch :+: g) (CarrierExc f g a)
 algExc = A a d p where
     a :: (Functor f, Functor g) => (Throw :+: f) (CarrierExc f g a n) -> CarrierExc f g a n
-    -- Don't perform continuation if an error is thrown.
-    a (Throw err) = Exc (return (Left err))
-    a (Other op)  = undefined
+    a (Throw err) = Exc $ \hdl ->
+        case hdl of
+            Nothing -> return (Left err)
+            Just h  -> return (Right (h err))
+    a (Other op) = Exc $ \hdl -> (Op (fmap (\(Exc run) -> run hdl) op))
 
     d :: (Functor f, Functor g) => (Catch :+: g) (CarrierExc f g a ('S n)) -> CarrierExc f g a n
-    d (Catch hdl) = undefined
+    d (Catch hdl k) = undefined
+
     d (Other op)  = undefined
 
     p :: CarrierExc f g a n -> CarrierExc f g a ('S n)
@@ -77,8 +76,7 @@ algExc = A a d p where
 -- Converts any program which might thrown an exception into a program that
 -- returns either its result, or an error.
 handleExc :: (Functor f, Functor g) => Prog (Throw :+: f) (Catch :+: g) a -> Prog f g (Either String a)
-handleExc prog = case run genExc algExc prog of
-    (Exc x) -> x
+handleExc prog = runExc (run genExc algExc prog) Nothing
 
 test :: Prog (Throw :+: Void) (Catch :+: Void) Int
 test = do
