50ea6df4c1878682291b9b635a447d9a64302bbf
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-01 11:00:46 +0100

    failing test

--- test/Transform/RenameSpec.hs
+++ test/Transform/RenameSpec.hs
@@ -21,11 +21,19 @@ getIVar = getVar
 getFVar :: VarExp Fresh :<: f => Fresh -> Prog f g a
 getFVar = getVar
 
+v :: Word -> Fresh
+v = Fresh "v"
+
 renameSpec :: Spec
 renameSpec = do
     describe "renaming" $ do
         context "variables" $ do
             it "renames getting variables" $ do
                 let a = getIVar "x" :: IWhile
-                    e = getFVar (Fresh "v" 0) :: FWhile
+                    e = getFVar (v 0) :: FWhile
+                rename a `shouldBe` e
+
+            it "renames variables in arithmetic expressions" $ do
+                let a = add (getIVar "x") (getIVar "y") :: IWhile
+                    e = add (getFVar (v 0)) (getFVar (v 1)) :: FWhile
                 rename a `shouldBe` e
