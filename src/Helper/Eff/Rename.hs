c4d76ef68a9fc2747b6a56c175cf103412818c82
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-03 11:12:11 +0100

    refactoring rename

--- src/Helper/Eff/Rename.hs
+++ src/Helper/Eff/Rename.hs
@@ -4,7 +4,7 @@
 -- to ensure procedures have been defined before being used.
 
 {-# LANGUAGE ViewPatterns, PatternSynonyms, TypeOperators, DeriveFunctor #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, DataKinds #-}
 
 module Transform.Rename.RenameEff
 ( FreshName(..)
@@ -24,6 +24,7 @@ import Helper.Pretty
 import Helper.Co
 import Helper.Eff.State
 import Helper.Eff.Fresh
+import Helper.Eff
 
 --------------------------------------------------------------------------------
 -- Syntax
@@ -35,18 +36,18 @@ data FreshName  = FreshName Prefix Word deriving Eq
 instance Pretty FreshName where
     pretty (FreshName pre i) = do text pre; showable i
 
-data Rename v k
+data Rename k
     -- Returns fresh name corresponding to renamed identifier. If the name
     -- is unseen then a fresh mapping is created.
-    = Name' v (FreshName -> k)
+    = Name' String (FreshName -> k)
     -- Returns whether a mapping exists.
-    | Exists' v (Bool -> k)
+    | Exists' String (Bool -> k)
     deriving Functor
 
-data LocalName v k
+data LocalName k
     -- Assigns fresh names to all supplied names, and uses these new names
     -- inside continuation. After local, original names are restored.
-    = Local' [v] k
+    = Local' [String] k
     deriving Functor
 
 -- Smart constructors
@@ -54,29 +55,54 @@ data LocalName v k
 -- Use pattern synonyms and view patterns suggested in Effect Handlers in Scope.
 -- These help make pattern matching in effect handler more readable.
 pattern Name v fk <- (prj -> Just (Name' v fk))
-name :: (Functor f, Functor g) => Rename v :<: f => v -> Prog f g FreshName
+name :: (Functor f, Functor g) => Rename :<: f => String -> Prog f g FreshName
 name v = inject (Name' v Var)
 
 pattern Exists p fk <- (prj -> Just (Exists' p fk))
-exists :: (Functor f, Functor g) => Rename v :<: f => v -> Prog f g Bool
+exists :: (Functor f, Functor g) => Rename :<: f => String -> Prog f g Bool
 exists p = inject (Exists' p Var)
 
 pattern Local vs k <- (prj -> Just (Local' vs k))
-localNames :: (Functor f, Functor g) => LocalName v :<: g => [v] -> Prog f g a -> Prog f g a
+localNames :: (Functor f, Functor g) => LocalName :<: g => [String] -> Prog f g a -> Prog f g a
 localNames vs inner = injectS (fmap (fmap return) (Local' vs inner))
 
 --------------------------------------------------------------------------------
 -- Semantics
 --------------------------------------------------------------------------------
 
-type Names v = Map v FreshName
+type Names = Map String FreshName
 
 -- Describe renaming in terms of State and FreshName effect handlers.
 
+type Carrier f g a = CarrierId (Prog (State Names :+: Fresh :+: f) (LocalSt Names :+: g) a)
+
+algRn :: (Functor f, Functor g) => Alg (Rename :+: f) (LocalName :+: g) (Carrier f g a)
+algRn = A a d p where
+    a :: (Functor f, Functor g) => (Rename :+: f) (Carrier f g a n) -> Carrier f g a n
+    a (Name v fk) = Id $ do
+        env <- get
+        case Map.lookup v env of
+            -- Mapping already exists, so just return it.
+            Just fresh -> unId (fk fresh)
+            -- No mapping exists, so create a new one.
+            Nothing -> do
+                next <- fresh
+                let f = FreshName "v" next
+                put (Map.insert v f env)
+                unId (fk f)
+
+    a (Other op) = undefined
+
+    d :: (Functor f, Functor g) => (LocalName :+: g) (Carrier f g a ('S n)) -> Carrier f g a n
+    d = undefined
+
+    p :: (Functor f, Functor g) => Carrier f g a n -> Carrier f g a ('S n)
+    p = undefined
+
 mkRename :: (Functor f, Functor g)
-         => Prog (Rename v :+: f) (LocalName v :+: g) a
-         -> Prog (State (Names v) :+: Fresh :+: f) (LocalSt (Names v) :+: g) a
+         => Prog (Rename :+: f) (LocalName :+: g) a
+         -> Prog (State Names :+: Fresh :+: f) (LocalSt Names :+: g) a
 mkRename = undefined
 
-handleRename :: (Functor f, Functor g) => Prog (Rename v :+: f) (LocalName v :+: g) a -> Prog f g a
+handleRename :: (Functor f, Functor g) => Prog (Rename :+: f) (LocalName :+: g) a -> Prog f g a
 handleRename = undefined
