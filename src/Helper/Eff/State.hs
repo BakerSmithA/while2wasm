64a30dffe6c8707b4cb0dc9a0e5d916474a2f756
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-03 09:39:19 +0100

    generic state

--- src/Helper/Eff/State.hs
+++ src/Helper/Eff/State.hs
@@ -5,7 +5,14 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ViewPatterns, PatternSynonyms #-}
 
-module Helper.Eff.State where
+module Helper.Eff.State
+( State
+, LocalSt
+, get
+, put
+, local
+, handleState
+) where
 
 import Helper.Prog
 import Helper.Co
@@ -23,9 +30,11 @@ data State s k
     | Put' s k
     deriving Functor
 
-data Local s k
-    -- Creates a block where state is local, and restored outside.
-    = Local' s k
+data LocalSt s k
+    -- Creates a block where state is local. State after the local block is
+    -- restored using the given function, which takes state before entering
+    -- local, and state at end of local, and returns state to be used after.
+    = Local' s (s -> s -> s) k
     deriving Functor
 
 pattern Get fk <- (prj -> Just (Get' fk))
@@ -36,9 +45,9 @@ pattern Put s k <- (prj -> Just (Put' s k))
 put :: (Functor f, Functor g, State s :<: f) => s -> Prog f g ()
 put s = inject (Put' s (Var ()))
 
-pattern Local s k <- (prj -> Just (Local' s k))
-local :: (Functor f, Functor g, Local s :<: g) => s -> Prog f g a -> Prog f g a
-local s inner = injectS (fmap (fmap return) (Local' s inner))
+pattern Local s combine k <- (prj -> Just (Local' s combine k))
+local :: (Functor f, Functor g, LocalSt s :<: g) => s -> (s -> s -> s) -> Prog f g a -> Prog f g a
+local s combine inner = injectS (fmap (fmap return) (Local' s combine inner))
 
 --------------------------------------------------------------------------------
 -- Semantics
@@ -54,15 +63,28 @@ data Carrier' f g s a :: Nat -> * where
 genSt :: (Functor f, Functor g) => a -> Carrier f g s a 'Z
 genSt x = St (\s -> return (CZ x, s))
 
-algSt :: (Functor f, Functor g) => Alg (State s :+: f) (Local s :+: g) (Carrier f g s a)
+algSt :: (Functor f, Functor g) => Alg (State s :+: f) (LocalSt s :+: g) (Carrier f g s a)
 algSt = A a d p where
     a :: (Functor f, Functor g) => (State s :+: f) (Carrier f g s a n) -> Carrier f g s a n
     a (Get fk)   = St $ \s -> runSt (fk s) s
     a (Put s' k) = St $ \_ -> runSt k s'
     a (Other op) = St $ \s -> Op (fmap (\(St run) -> run s) op)
 
-    d :: (Functor f, Functor g) => (Local s :+: g) (Carrier f g s a ('S n)) -> Carrier f g s a n
-    d = undefined
+    d :: (Functor f, Functor g) => (LocalSt s :+: g) (Carrier f g s a ('S n)) -> Carrier f g s a n
+    d (Local s' combine k) = St $ \s -> do
+        -- Run nested continuation with inner-state
+        (CS run', s'') <- runSt k s'
+        -- Run after nested continuation with original state combined with inner state.
+        run' (combine s s'')
+
+    d (Other op) = St $ \s -> Scope (fmap (\(St run) -> fmap f (run s)) op) where
+        f :: (Carrier' f g s a ('S n), s) -> Prog f g (Carrier' f g s a n, s)
+        f (CS run, s) = run s
 
     p :: (Functor f, Functor g) => Carrier f g s a n -> Carrier f g s a ('S n)
-    p = undefined
+    p (St run) = St $ \s -> return (CS run, s)
+
+handleState :: (Functor f, Functor g) => s -> Prog (State s :+: f) (LocalSt s :+: g) a -> Prog f g (a, s)
+handleState s prog = do
+    (CZ prog', s') <- runSt (run genSt algSt prog) s
+    return (prog', s') 
