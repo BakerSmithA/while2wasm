37993e2fd26f1142fb9459cb69934a6ff284a7ef
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-04 21:56:39 +0100

    started codegen

---
+++ src/Back/CodeGen.hs
@@ -0,0 +1,39 @@
+
+-- Generating WebAssembly from processed While.
+-- Demonstrates utility of scoping when compiling to a language which has
+-- scoped constructs.
+
+{-# LANGUAGE DeriveFunctor #-}
+
+module Back.CodeGen where
+
+import Back.WASM
+
+data Emit v p k
+    -- Append an instruction to the end of the current block at innermost scope.
+    = Emit (WASM ()) k
+    -- Get instructions of innermost scoped block.
+    | GetInstr (WASM () -> k)
+    -- Get name of stack pointer, which can be used to load variables from
+    -- function's stack frame.
+    | GetSP (GlobalName -> k)
+    -- Get offset of a local variable from the stack pointer.
+    | GetSPOffset v (Word -> k)
+    -- Returns names of arguments to a function, i.e. the variables in
+    -- the current scope that should be pushed onto the stack before calling
+    -- the function.
+    | GetArgs p ([v] -> k)
+    -- Returns the type of a variable, in the current function, given its name.
+    -- This informs how the variable should be accessed to push into the stack
+    -- before calling another function.
+    | GetVarType v (v -> k)
+    deriving Functor
+
+data Block v k
+    -- Create a new block of instructions that nested emits will append to.
+    -- Once scope is exited, this block will be popped from the stack.
+    = CodeBlock k
+    -- Create a new block of instructions that nested emits will append to.
+    -- Once scope is exited, the instructions will be placed in a function.
+    | Function FuncName [v] Bool k
+    deriving Functor
