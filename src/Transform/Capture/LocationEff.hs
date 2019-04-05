68015165a19ba1d4776ea2c8c7a067cf9742f65d
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-05 10:35:58 +0100

    Added location effect handler but not tested

--- src/Transform/Capture/LocationEff.hs
+++ src/Transform/Capture/LocationEff.hs
@@ -5,14 +5,27 @@
 -- WARNING: Assumes all variable names are unique.
 
 {-# LANGUAGE DeriveFunctor, TypeOperators, FlexibleContexts #-}
-{-# LANGUAGE ViewPatterns, PatternSynonyms #-}
+{-# LANGUAGE ViewPatterns, PatternSynonyms, DataKinds, KindSignatures, GADTs #-}
 
-module Transform.Capture.LocationEff where
+module Transform.Capture.LocationEff
+( Location(..)
+, LocOp
+, Add
+, Discard
+, seen
+, addLocals
+, discardLocals
+, handleLoc
+) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Helper.Prog
 import Helper.Co
+import Helper.Eff.Reader
+import Helper.Eff.State
+import Helper.Eff
 
 data Location
     -- Variable local to a level of scope, analagous to a variable decalared
@@ -22,6 +35,10 @@ data Location
     -- passed into a function.
     | Foreign
     deriving (Eq, Show)
+
+--------------------------------------------------------------------------------
+-- Syntax
+--------------------------------------------------------------------------------
 
 data LocOp v k
     -- Tells the environment that variable v was seen at the current scope.
@@ -64,3 +81,112 @@ discardLocals inner = injectS (fmap (fmap return) (Discard' f)) where
         x  <- inner
         ls <- getLocations
         return (x, ls)
+
+--------------------------------------------------------------------------------
+-- Semantics
+--------------------------------------------------------------------------------
+
+-- Whether a variable was declared at the current scope.
+type IsLocal v = v -> Bool
+
+-- Simple shallow DSL to describe which variables are local.
+allLocal :: IsLocal v
+allLocal = const True
+
+noneLocal :: IsLocal v
+noneLocal = const False
+
+isLocalFromList :: Ord v => [v] -> IsLocal v
+isLocalFromList vs v = v `Set.member` Set.fromList vs
+
+orLocal :: IsLocal v -> IsLocal v -> IsLocal v
+orLocal x y v = x v || y v
+
+type Op  f v     = Ask    (IsLocal v) :+: State   (Map v Location) :+: f
+type Sc  g v     = LocalR (IsLocal v) :+: LocalSt (Map v Location) :+: g
+type Hdl f g v a = Prog (Op f v) (Sc g v) a
+
+data Carrier f g v a n
+    = Lc { runL :: Hdl f g v (Carrier' f g v a n) }
+
+data Carrier' f g v a :: Nat -> * where
+    CZ :: a -> Carrier' f g v a 'Z
+    CS :: (Hdl f g v (Carrier' f g v a n)) -> Carrier' f g v a ('S n)
+
+getCurrVarLoc :: (Functor f, Functor g) => v -> Hdl f g v Location
+getCurrVarLoc v = do
+    isLocal <- ask
+    return $ if isLocal v then Local else Foreign
+
+getAllVarLocs :: (Functor f, Functor g) => Hdl f g v (Map v Location)
+getAllVarLocs = get
+
+putAllVarLocs :: (Functor f, Functor g) => Map v Location -> Hdl f g v ()
+putAllVarLocs = put
+
+updateVarLoc :: (Functor f, Functor g, Ord v) => v -> Location -> Hdl f g v ()
+updateVarLoc v loc = do
+    locs <- getAllVarLocs
+    putAllVarLocs (Map.insert v loc locs)
+
+getIsLocal :: (Functor f, Functor g) => Hdl f g v (IsLocal v)
+getIsLocal = ask
+
+-- TODO: How to remove these?
+noneLocal' :: (Functor f, Functor g) => Hdl f g v (IsLocal v)
+noneLocal' = return noneLocal
+
+emptyLocs :: (Functor f, Functor g) => Hdl f g v (Map v Location)
+emptyLocs = return Map.empty
+
+genL :: (Functor f, Functor g) => a -> Carrier f g v a 'Z
+genL x = Lc (return (CZ x))
+
+algL :: (Functor f, Functor g, Ord v) => Alg (LocOp v :+: f) (Add v :+: Discard :+: g) (Carrier f g v a)
+algL = A a d p where
+    a :: (Functor f, Functor g, Ord v) => (LocOp v :+: f) (Carrier f g v a n) -> Carrier f g v a n
+    a (Seen v k) = Lc $ do
+        -- Update map to contain whether variable is local or foreign at current scope.
+        loc <- getCurrVarLoc v
+        updateVarLoc v loc
+        runL k
+    a (GetLocations fk) = Lc $ do
+        locs <- getAllVarLocs
+        runL (fk locs)
+    a (Other op) = Lc (Op (fmap runL (R $ R op)))
+
+    d :: (Functor f, Functor g, Ord v) => (Add v :+: Discard :+: g) (Carrier f g v a ('S n)) -> Carrier f g v a n
+    d (Add vs k) = Lc $ do
+        isLocal <- getIsLocal
+        let vsIsLocal = isLocalFromList vs
+        -- Inside local block, both the original and new variables are local.
+        (CS run') <- localR (isLocal `orLocal` vsIsLocal) (runL k)
+        -- Original locals are restored by local reader.
+        run'
+    d (Discard k) = Lc $ do
+        -- TODO: How to make this work outwith ugly-ness. Problem with types regarding v.
+        n <- noneLocal'
+        m <- emptyLocs
+        -- Run continuation with no local variables, as is semantics of discard.
+        (CS run') <- localR n (do
+            -- Remove all mappings from variables to locations in inner scope,
+            -- as is semantics of discard.
+            localSt m (runL k))
+        -- Original locals and mapping is restored through scoping.
+        run'
+    d (Other (Other op)) = Lc (Scope (fmap (\(Lc prog) -> fmap f prog) (R $ R op))) where
+        f :: (Functor f, Functor g) => Carrier' f g v a ('S n) -> Hdl f g v (Carrier' f g v a n)
+        f (CS prog) = prog
+
+    p :: (Functor f, Functor g, Ord v) => Carrier f g v a n -> Carrier f g v a ('S n)
+    p (Lc runL) = Lc (return (CS runL))
+
+mkLoc :: (Functor f, Functor g, Ord v) => Prog (LocOp v :+: f) (Add v :+: Discard :+: g) a -> Hdl f g v a
+mkLoc prog = case run genL algL prog of
+    (Lc prog') -> do
+        (CZ x) <- prog'
+        return x
+
+-- Returns result and mapping from variable names to locations at the top level scope.
+handleLoc :: (Functor f, Functor g, Ord v) => Prog (LocOp v :+: f) (Add v :+: Discard :+: g) a -> Prog f g (a, Map v Location)
+handleLoc = handleState Map.empty . handleReader allLocal . mkLoc
--- test/Test.hs
+++ test/Test.hs
@@ -8,7 +8,7 @@ import Helper.Eff.FreshSpec
 import Helper.Eff.ReaderSpec
 import Helper.Eff.WriterSpec
 import Transform.Rename.RenameEffSpec
-import Transform.Capture.StoreEffSpec
+-- import Transform.Capture.StoreEffSpec
 import Transform.Capture.DirtyEffSpec
 import Transform.RenameSpec
 
@@ -23,5 +23,5 @@ main = hspec specs where
         renameEffSpec
         renameSpec
         writerSpec
-        storeEffSpec
+        -- storeEffSpec
         dirtyEffSpec
