0b9ffd9ed5024035911efffab73d6b64cf83a05c
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-09 20:41:49 +0100

    renames AST

--- src/Transform/Rename/Rename.hs
+++ src/Transform/Rename/Rename.hs
@@ -21,69 +21,64 @@ import Helper.Eff.Void
 import Front.Pretty
 import Helper.Pretty
 
+-- To allow two versions of Fresh and Rename to exist in context, i.e. for
+-- variables and procedures, need to be able to distinguish between them.
+newtype VarName  = VarName  Ident deriving (Eq, Ord, Show)
+newtype ProcName = ProcName Ident deriving (Eq, Ord, Show)
+
 -- Context in which renaming is performed. This provides fresh names and scope
 -- for renaming.
-type Op  = Fresh  Ident :+: Void
-type Sc  = Rename Ident :+: Void
+type Op  = Fresh  VarName :+: Fresh  ProcName :+: Void
+type Sc  = Rename VarName :+: Rename ProcName :+: Void
 type Ctx = Prog Op Sc
 
 instance VarExp FreshName :<: f => FreeAlg (VarExp Ident) (Ctx (Free f a)) where
     alg (GetVar v) = do
-        v' <- fresh v
+        v' <- fresh (VarName v)
         return (getVar v')
 
 instance AExp :<: f => FreeAlg AExp (Ctx (Free f a)) where
-    alg = undefined
+    alg (Num n)   = return (num n)
+    alg (Add x y) = add <$> x <*> y
+    alg (Sub x y) = sub <$> x <*> y
+    alg (Mul x y) = mul <$> x <*> y
 
 instance BExp :<: f => FreeAlg BExp (Ctx (Free f a)) where
-    alg = undefined
+    alg (T)       = return true
+    alg (F)       = return false
+    alg (Equ x y) = equ  <$> x <*> y
+    alg (LEq x y) = leq  <$> x <*> y
+    alg (And x y) = andB <$> x <*> y
+    alg (Not x)   = notB <$> x
 
 instance VarStm FreshName :<: f => FreeAlg (VarStm Ident) (Ctx (Free f a)) where
     alg (SetVar v x) = do
-        v' <- fresh v
+        v' <- fresh (VarName v)
         rnX <- x
         return (setVar v' rnX)
 
 instance ProcStm FreshName :<: f => FreeAlg (ProcStm Ident) (Ctx (Free f a)) where
-    alg = undefined
+    alg (Call pname) = do
+        pname' <- fresh (ProcName pname)
+        return (call pname')
 
 instance Stm :<: f => FreeAlg Stm (Ctx (Free f a)) where
-    alg (While cond body) = while <$> cond <*> body
-    alg (Comp s1 s2)      = comp <$> s1 <*> s2
+    alg (Skip)            = return skip
+    alg (Export x)        = export <$> x
+    alg (If cond t e)     = ifElse <$> cond <*> t <*> e
+    alg (While cond body) = while  <$> cond <*> body
+    alg (Comp s1 s2)      = comp   <$> s1 <*> s2
 
 instance BlockStm FreshName FreshName :<: f => FreeAlg (BlockStm Ident Ident) (Ctx (Free f a)) where
-    alg = undefined
+    alg (Block varDecls procDecls body) = rename (map VarName (fsts varDecls)) $ do
+        rnVarDecls  <- map2M (fresh . VarName) id varDecls
+        rnProcDecls <- map2M (fresh . VarName) id procDecls
+        rnBody      <- body
+        return (block rnVarDecls rnProcDecls rnBody)
 
 makeRename :: (Functor g, FreeAlg f (Ctx (Free g a))) => Free f a -> Ctx (Free g a)
 makeRename = evalF (return . return)
 
 renameAST :: (Functor g, FreeAlg f (Ctx (Free g a))) => Free f a -> Free g a
-renameAST = handleVoid . handleRename . makeRename
-
-test :: While Ident Ident
-test = do
-    while (getVar "x") (do
-        setVar "x" (getVar "y"))
-
-runTest :: IO ()
-runTest = do
-    let rn = renameAST test :: While FreshName FreshName
-    putStrLn (toString 0 $ docAST test)
-    putStrLn ("-- After-- ")
-    putStrLn (toString 0 $ docAST rn)
-
--- import Transform.Rename.AST
--- import Transform.Rename.RenameEff
--- import Helper.Scope.Prog
--- import Helper.Scope.Alg
--- import Helper.Eff.Void
--- import Helper.Eff.Exception
---
--- -- Use extensible effects methods to run handlers.
--- handle :: RenameHandler h i -> Either String (Prog h i ())
--- handle = handleVoid . handleRename . handleRename . handleExc
---
--- rename :: (Functor h, Functor i)
---        => (OpAlg f (Carrier h i), ScopeAlg g (Carrier h i))
---        => Prog f g () -> Either String (Prog h i ())
--- rename = handle . makeRn
+-- Handle renaming variable and procedures.
+renameAST = handleVoid . handleRename . handleRename . makeRename
