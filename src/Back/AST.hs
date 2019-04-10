c10863baef80f952e6ebfa695a794fee0e070f49
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-10 14:42:10 +0100

    WASM emit convenience functions

--- src/Back/AST.hs
+++ src/Back/AST.hs
@@ -1,4 +1,23 @@
 
 -- Conversion from While AST to CodeGen to generate WebAssembly.
 
+{-# LANGUAGE TypeOperators, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Back.AST where
+
+import Front.AST
+import Back.CodeGen
+import Back.WASM
+import Helper.Free.Free
+import Helper.Free.Alg
+import Helper.Scope.Prog
+import Helper.Co
+import Helper.Eff.Void
+
+type Op      = Emit  :+: Void
+type Sc      = Block :+: Void
+type Carrier = Prog Op Sc ()
+
+instance FreeAlg (VarExp SrcVar) Carrier where
+    alg (GetVar v) = undefined
--- src/Back/CodeGen.hs
+++ src/Back/CodeGen.hs
@@ -5,7 +5,24 @@
 
 {-# LANGUAGE DeriveFunctor, TypeOperators, FlexibleContexts #-}
 
-module Back.CodeGen where
+module Back.CodeGen
+( SrcVar
+, SrcProc
+, LocType(..)
+, ValType(..)
+, Emit
+, Block
+, emit
+, spName
+, varSPOffset
+, callerScopeFuncArgs
+, varType
+, codeBlock
+, function
+, emitGetVarAsArg
+, emitGetVarVal
+, emitSetVarVal
+) where
 
 import Transform.Rename.Rename (FreshName)
 import Back.WASM
@@ -26,14 +43,14 @@ type SrcProc = FreshName
 data LocType v
     = Local v
     | Param v
-    deriving (Eq, Show)
+    deriving (Eq, Show, Functor)
 
 -- Whether variable the value of a variable is stored in a local variable, or
 -- on the stack.
 data ValType v
-    = Value v
+    = Val v
     | Ptr v
-    deriving (Eq, Show)
+    deriving (Eq, Show, Functor)
 
 data Emit k
     -- Append an instruction to the end of the current block at innermost scope.
@@ -45,7 +62,7 @@ data Emit k
     -- function's stack frame.
     | SPName (GlobalName -> k)
     -- Get offset of a local variable from the stack pointer.
-    | SPOffset SrcVar (Word -> k)
+    | VarSPOffset SrcVar (Word -> k)
     -- Returns names of arguments to a function, i.e. the variables in
     -- the **caller** scope that should be pushed onto the stack before calling
     -- the function.
@@ -73,8 +90,8 @@ currInstr = injectP (CurrInstr Var)
 spName :: Emit :<: f => Prog f g GlobalName
 spName = injectP (SPName Var)
 
-spOffset :: Emit :<: f => SrcVar -> Prog f g Word
-spOffset v = injectP (SPOffset v Var)
+varSPOffset :: Emit :<: f => SrcVar -> Prog f g Word
+varSPOffset v = injectP (VarSPOffset v Var)
 
 callerScopeFuncArgs :: Emit :<: f => SrcProc -> Prog f g [SrcVar]
 callerScopeFuncArgs pname = injectP (CallerScopeFuncArgs pname Var)
@@ -90,3 +107,48 @@ codeBlock inner = injectPSc (fmap (fmap return) (Block (do inner; currInstr)))
 -- Emits nested instructions to a new function.
 function :: (Functor f, Block :<: g) => SrcProc -> [SrcVar] -> DoesRet -> Prog f g a -> Prog f g a
 function name args ret body = injectPSc (fmap (fmap return) (Function name args ret body))
+
+--------------------------------------------------------------------------------
+-- Convenience functions to make converting from While easier
+--------------------------------------------------------------------------------
+
+-- Converts source-code name into name that can be emitted into WASM.
+wasmName :: SrcVar -> LocalName
+wasmName = show
+
+-- Emit instructions to push an argument to another function onto the stack.
+-- I.e. address of a pointer, or value of a value.
+emitGetVarAsArg :: (Functor g, Emit :<: f) => LocType (ValType (SrcVar)) -> Prog f g ()
+emitGetVarAsArg (Local (Val v)) = emit (getLocal (wasmName v))
+emitGetVarAsArg (Param (Val v)) = emit (getLocal (wasmName v))
+emitGetVarAsArg (Local (Ptr v)) = emitLocalPtrAddr v
+emitGetVarAsArg (Param (Ptr v)) = emit (getLocal (wasmName v))
+
+-- Emit instuction to push the value of a variable onto the stack.
+-- I.e. deference a pointer, or value of a value.
+emitGetVarVal :: (Functor g, Emit :<: f) => LocType (ValType SrcVar) -> Prog f g ()
+emitGetVarVal (Local (Val v)) = emit (getLocal (wasmName v))
+emitGetVarVal (Param (Val v)) = emit (getLocal (wasmName v))
+emitGetVarVal (Local (Ptr v)) = do emitLocalPtrAddr v; emit (load 0)
+emitGetVarVal (Param (Ptr v)) = do emit (getLocal (wasmName v)); emit (load 0)
+
+-- Pushes address of a local varible onto the stack, i.e. some offset from SP.
+emitLocalPtrAddr :: (Functor g, Emit :<: f) => SrcVar -> Prog f g ()
+emitLocalPtrAddr v = do
+    sp     <- spName
+    offset <- varSPOffset v
+    emit (getGlobal sp)
+    emit (constNum (fromIntegral offset))
+    emit (binOp SUB)
+
+-- Emit instruction to set value of a variable to value on top of stack.
+-- I.e. store value at memory address pointed to, or set value of variable.
+emitSetVarVal :: (Functor g, Emit :<: f) => LocType (ValType SrcVar) -> Prog f g () -> Prog f g ()
+emitSetVarVal (Local (Val v)) val = do val; emit (setLocal (wasmName v))
+emitSetVarVal (Param (Val v)) val = do val; emit (setLocal (wasmName v))
+emitSetVarVal (Local (Ptr v)) val = emitSetPtr (emitLocalPtrAddr v) val
+emitSetVarVal (Param (Ptr v)) val = emitSetPtr (emit (getLocal (wasmName v))) val
+
+-- Sets the value pointed to by a pointer.
+emitSetPtr :: (Functor g, Emit :<: f) => Prog f g () -> Prog f g () -> Prog f g ()
+emitSetPtr addr val = do addr; val; emit (store 0)
