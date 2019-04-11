ae46df87404a23ceaeca09bedb56448f5b31b37e
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-11 16:21:51 +0100

    subbed codegen semantics

--- src/Back/CodeGenSem.hs
+++ src/Back/CodeGenSem.hs
@@ -11,8 +11,28 @@ import Back.WASM
 import Back.CodeGenSyntax
 import Helper.Scope.Prog
 
-handleCodeGen :: Prog GenData Function WASM -> (WASM, [Func])
-handleCodeGen = undefined
+data Env = Env
 
--- handleCodeGen prog = case runCG (run gen alg prog) emptyGenEnv of
---     (_, env) -> peekBlock (blocks env)
+data Carrier n = CG { runCG :: Env -> (Carrier' n, Env) }
+
+data Carrier' :: Nat -> * where
+    CZ :: WASM -> Carrier' 'Z
+    CS :: (Env -> (Carrier' n, Env)) -> Carrier' ('S n)
+
+gen :: WASM -> Carrier 'Z
+gen wasm = CG (\env -> (CZ wasm, env))
+
+alg :: Alg GenData Function Carrier
+alg = A a d p where
+    a :: GenData (Carrier n) -> Carrier n
+    a = undefined
+
+    d :: Function (Carrier ('S n)) -> Carrier n
+    d = undefined
+
+    p :: Carrier n -> Carrier ('S n)
+    p = undefined
+
+handleCodeGen :: Prog GenData Function WASM -> (WASM, [Func])
+handleCodeGen prog = case runCG (run gen alg prog) Env of
+    (CZ wasm, env) -> (wasm, [])
