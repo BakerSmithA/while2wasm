9a3d8dcf02f2cb23c43cd50e642d81a824c8e8fa
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-21 18:31:50 +0100

    removed comments

--- src/Helper/Eff/Writer.hs
+++ src/Helper/Eff/Writer.hs
@@ -74,28 +74,3 @@ handleWriter prog = case handleWriter' prog of
     (W prog') -> do
         (CZ x, w) <- prog'
         return (x, w)
-
-
--- No scoping, therefore can use CarrierId.
--- type Carrier f g w a = CarrierId (Prog f g (a, w))
---
--- genW :: (Functor f, Functor g, Monoid w) => a -> Carrier f g w a 'Z
--- genW x = Id (return (x, mempty))
---
--- algW :: (Functor f, Functor g, Monoid w) => Alg (Tell w :+: f) g (Carrier f g w a)
--- algW = A a d p where
---     a :: (Functor f, Functor g, Monoid w) => (Tell w :+: f) (Carrier f g w a n) -> Carrier f g w a n
---     a (Tell w k) = Id $ do
---         (x, w') <- unId k
---         return (x, w `mappend` w')
---
---     a (Other op) = Id (Op (fmap unId op))
---
---     d :: (Functor f, Functor g) => g (Carrier f g w a ('S n)) -> Carrier f g w a n
---     d op = Id $ (Scope (fmap (return . unId) op))
---
---     p :: (Functor f, Functor g) => Carrier f g w a n -> Carrier f g w a ('S n)
---     p (Id p) = Id p
---
--- handleWriter :: (Functor f, Functor g, Monoid w) => Prog (Tell w :+: f) g a -> Prog f g (a, w)
--- handleWriter = runId genW algW
