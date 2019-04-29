b6df7e429e6b4687a8ab004e28eb1f32fe93ba38
Albie Baker-Smith <bakersmitha@gmail.com>
2019-04-29 19:11:26 +0100

    gen

--- src/Gen.hs
+++ src/Gen.hs
@@ -2,13 +2,17 @@
 -- Since Megaparsec is not built for profiling, need to manually generate
 -- While which can be compiled.
 
-module Gen (prog) where
+module Gen where
 
 import Front.Parse.Rec
 
 block :: Stm -> Stm
 block body = Block [("x", Num 1)] [("p", body)] body
 
-prog :: Int -> Stm
-prog 0 = Export (Ident "x")
-prog n = block (prog (n-1))
+progRec :: Int -> Stm
+progRec 0 = Export (Ident "x")
+progRec n = block (progRec (n-1))
+
+progLin :: Int -> Stm
+progLin 0 = Export (Ident "x")
+progLin n = Assign "x" (Num 1) `Comp` (progLin (n-1))
