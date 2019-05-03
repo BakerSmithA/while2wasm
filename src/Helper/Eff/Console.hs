d87bff8bc6619349031c44e4ec9e6753e1affd0a
Albie Baker-Smith <bakersmitha@gmail.com>
2019-05-03 21:25:10 +0100

    progT2

--- src/Helper/Eff/Console.hs
+++ src/Helper/Eff/Console.hs
@@ -7,6 +7,8 @@
 module Helper.Eff.Console where
 
 import Helper.Scope.Prog
+import Helper.Scope.ProgT (ProgT)
+import qualified Helper.Scope.ProgT as T
 import Helper.Co
 import Helper.Inj
 import Helper.Eff
@@ -33,27 +35,30 @@ puts s = injectP (Puts' s (Var ()))
 -- Semantics
 --------------------------------------------------------------------------------
 
-data Carrier f g a n = C { runC :: Prog f g (IO (Carrier' f g a n)) }
+data Carrier f g a n = C { runC :: ProgT f g IO (Carrier' f g a n) }
 
 data Carrier' f g a :: Nat -> * where
     CZ :: a -> Carrier' f g a 'Z
-    CS :: Prog f g (IO (Carrier' f g a n)) -> Carrier' f g a ('S n)
+    CS :: ProgT f g IO (Carrier' f g a n) -> Carrier' f g a ('S n)
 
-gen :: a -> Carrier f g a 'Z
-gen = undefined
+handleConsole :: ProgT (Console :+: f) g IO a -> IO (Carrier f g a 'Z)
+handleConsole prog = T.run undefined undefined prog
 
-alg :: Alg (Console :+: f) g (Carrier f g a)
-alg = A a undefined undefined where
-    a :: (Console :+: f) (Carrier f g a n) -> Carrier f g a n
-    a (Gets fk) = C $ return $ do
-        s <- getLine
-        _
-
-handleConsole :: (Functor f, Functor g) => Prog (Console :+: f) g a -> Prog f g (IO a)
-handleConsole prog =
-    case run gen alg prog of
-        C x -> fmap unZ x where
-            unZ :: IO (Carrier' f g a 'Z) -> IO a
-            unZ y = do
-                CZ y' <- y
-                return y'
+-- gen :: a -> Carrier f g a 'Z
+-- gen = undefined
+--
+-- alg :: Alg (Console :+: f) g (Carrier f g a)
+-- alg = A a undefined undefined where
+--     a :: (Console :+: f) (Carrier f g a n) -> Carrier f g a n
+--     a (Gets fk) = C $ return $ do
+--         s <- getLine
+--         _
+--
+-- handleConsole :: (Functor f, Functor g) => Prog (Console :+: f) g a -> Prog f g (IO a)
+-- handleConsole prog =
+--     case run gen alg prog of
+--         C x -> fmap unZ x where
+--             unZ :: IO (Carrier' f g a 'Z) -> IO a
+--             unZ y = do
+--                 CZ y' <- y
+--                 return y'
